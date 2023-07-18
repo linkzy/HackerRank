bf7e34b0eb5d89331040027c9ee5067d9e748fd7
Danilo Cavalcante <danilo.cavalcante@adm.unip.br>
2023-07-18 10:34:16 -0300

    adding sherlock holmes and the beast

---
+++ Algorithms/Greedy.cs
@@ -0,0 +1,52 @@
+﻿namespace HackerRank.Algorithms
+{
+    public static class Greedy
+    {
+        public static void decentNumber(int n)
+        {
+            if (new List<int> { 1, 2, 4 }.Contains(n))
+            {
+                Console.WriteLine("-1");
+                return;
+            }                
+
+            if (n % 3 == 0)
+            {
+                Console.WriteLine("".PadLeft(n, '5'));
+                return;
+            }
+            
+            for(int i = 5; i < n; i += 5)
+            {
+                int left = n - i;
+                if(left % 3 == 0)
+                {
+                    string number = "".PadLeft(left, '5');
+                    number += "".PadLeft(i, '3');
+                    Console.WriteLine(number);
+                    return;
+                }
+            }
+
+            for (int i = 3; i < n; i += 3)
+            {
+                int left = n - i;
+                if (left % 5 == 0)
+                {
+                    string number = "".PadRight(left, '3');
+                    Console.WriteLine(number.PadRight(i, '5'));
+                    return;
+                }
+            }
+
+
+            if (n % 5 == 0)
+            {
+                Console.WriteLine("".PadLeft(n, '3'));
+                return;
+            }
+
+            Console.WriteLine("-1");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿using Algorithms;
+using HackerRank.Algorithms;
 
 //Sorting.insertionSort2(6, new List<int>{ 9, 8, 6, 7, 3, 5, 4, 1, 2 });
-Implmentation.bonAppetit(new List<int>{3, 10, 2, 9}, 1, 7);
+//Implmentation.bonAppetit(new List<int>{3, 10, 2, 9}, 1, 7);
+Greedy.decentNumber(1);
+Greedy.decentNumber(3);
+Greedy.decentNumber(5);
+Greedy.decentNumber(11);
